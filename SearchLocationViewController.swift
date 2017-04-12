17daaf3f667bf95f2ff4e065268b7628806df2e9
HoangPhan <hoangphandev@gmail.com>
2017-04-13 00:28:07 +0700

    no message

--- SearchLocationViewController.swift
+++ SearchLocationViewController.swift
@@ -12,7 +12,7 @@ class SearchLocationViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //commit ngay moi
         // Do any additional setup after loading the view.
     }
 
